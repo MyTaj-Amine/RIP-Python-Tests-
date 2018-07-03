3c83a7b3180a49983c5c75fa1ea99b61d728f40b
Jesús <jcsombria@gmail.com>
2018-07-03 17:57:06 +0200

    Add reference implementation RIPGeneric.py

--- HttpServer.py
+++ HttpServer.py
@@ -9,23 +9,75 @@ import cherrypy
 import ujson
 import time
 import os
-from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
-from ws4py.websocket import WebSocket, EchoWebSocket
 
-from rip.RIPOctave import RIPOctave
+from rip.RIPGeneric import RIPGeneric
+from rip.RIPMeta import *
 
 class Root(object):
   exposed = True
-  server = RIPOctave()
+  control = RIPGeneric()
+  experiences = [
+    { 'id':'TestOK' },
+  ]
+  host = '< host_ip >:2055'
 
+  @cherrypy.expose
+  def index(self, expId=None):
+    '''
+    GET - Retrieve the list of experiences or information about a specific experience
+    '''
+    if expId is not None:
+      if expId in [e['id'] for e in self.experiences]:
+        response = self.control.info()
+      else:
+        response = '{}'
+    else:
+      response = self.info()
+    return response.encode("utf-8")
+
+  @cherrypy.expose
+  def SSE(self, expId=None):
+    '''
+    SSE - Connect to an experience's SSE channel to receive periodic updates
+    '''
+    cherrypy.response.headers['Content-Type'] = 'text/event-stream'
+    cherrypy.response.headers['Cache-Control'] = 'no-cache'
+    cherrypy.response.headers['Connection'] = 'keep-alive'
+
+    return self.control.nextSample()
+  SSE._cp_config = {'response.stream': True}
+
+  @cherrypy.expose
   @cherrypy.tools.accept(media='application/json')
   def POST(self):
+    '''
+    POST - JSON-RPC control commands (get/set)
+    '''
     socket = cherrypy.request.body.fp
     message = socket.read()
-    print(message)
-    response = self.server.parse(message)
-    print(response)
+    response = self.control.parse(message)
     return response.encode("utf-8")
+
+  def info(self):
+    '''
+    Build server info string
+    '''
+    params_get_info = [
+      RIPParam(name='Accept',required='no',location='header',value='application/json'),
+      RIPParam(name='expId',required='no',location='query',type_='string'),
+    ]
+    get_info = RIPMethod(
+      url='http://%s/RIP' % self.host, 
+      description='Retrieves information (variables and methods) of the experiences in the server',
+      type_='GET',
+      params=params_get_info,
+      returns='application/json',
+      example='http://%s/RIP?expId=TestOK' % self.host
+    )
+    meta = RIPExperienceList(self.experiences, [get_info])
+    return str(meta)
+
+# --------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
   # Provides a relative log path from the original script path
@@ -41,7 +93,6 @@ if __name__ == '__main__':
   })
   config = {
     '/': {
-      'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
       'tools.sessions.on': True,
       'tools.response_headers.on': True,
       'tools.response_headers.headers': [
@@ -53,4 +104,4 @@ if __name__ == '__main__':
     },
   }
 
-  cherrypy.quickstart(Root(), '/', config)
+  cherrypy.quickstart(Root(), '/RIP', config)
---
+++ rip/RIPGeneric.py
@@ -0,0 +1,212 @@
+'''
+@author: jcsombria
+'''
+import os.path
+import time
+
+from jsonrpc.JsonRpcServer import JsonRpcServer
+from jsonrpc.JsonRpcBuilder import JsonRpcBuilder
+from rip.RIPMeta import *
+
+builder = JsonRpcBuilder()
+
+class RIPGeneric(JsonRpcServer):
+  '''
+  RIP Server - Reference Implementation
+  '''
+
+  def __init__(self, name='RIP Server', description='Generic RIP Server Implementation.', authors='', keywords=''):
+    '''
+    Constructor
+    '''
+    super().__init__(name, description)
+
+    self.authors = authors
+    self.keywords = keywords
+    self.ssePeriod = 0.5
+    self.sseRunning = False
+    self.readables = [{
+        "name":"time",
+        "description":"Server time in seconds",
+        "type":"float",
+        "min":"0",
+        "max":"Inf",
+        "precision":"0"
+      },
+    ]
+    self.writables = []
+    methods = {
+      'get': { 'description': 'To read server variables',
+        'params': { 'expId': 'string', 'variables': '[string]' },
+        'implementation': self.get,
+      },
+      'set': { 'description': 'To write server variables',
+        'params': { 'expId': 'string', 'variables': '[string]', 'values':'[]' },
+        'implementation': self.set,
+      },
+    }
+    for method in methods:
+      try:
+        implementation = info[method].get('implementation')
+        self.on(method, info[method], implementation)
+      except:
+        print('[WARNING] Ignoring invalid method')
+
+  def info(self):
+    '''
+    Retrieve the experience's info
+    '''
+    try:
+      info = self.info_string
+    except:
+      info = self.build_info()
+      self.info_string = info
+    return info
+
+  def set(self, expid, variables, values):
+    '''
+    Sends one or more variables to the server
+    '''
+    pass
+
+  def get(self, expid, variables):
+    '''
+    Retrieve one or more variables from the server
+    '''
+    pass
+
+  def nextSample(self, wrap='data: %s\n\n'):
+    '''
+    Retrieve the next periodic update
+    '''
+    if not self.sseRunning:
+      self.sseRunning = True
+      self.sampler = Sampler(self.ssePeriod)
+
+    while self.sseRunning:
+      self.sampler.wait()
+      try:
+        toReturn = self.getValuesToNotify()
+      except:
+        toReturn = "ERROR"
+      response = builder.response(result=toReturn, request_id='1')
+      yield 'data: %s\n\n' % ujson.dumps(response)
+
+  def getValuesToNotify(self):
+    return [
+      [ "time" ],
+      [ self.sampler.lastTime() ]
+    ]
+
+  def build_info(self):
+    '''
+    Generate the experience's info string
+    '''
+    info = RIPServerInfo(
+      self.name,
+      self.description,
+      authors=self.authors,
+      keywords=self.keywords
+    )
+    readables = RIPVariablesList(
+      list_=self.readables,
+      methods=[self.buildSSEGetInfo(), self.buildPOSTGetInfo()],
+      read_notwrite=True
+    )
+    writables = RIPVariablesList(
+      list_=self.writables,
+      methods=[self.buildPOSTSetInfo()],
+      read_notwrite=False
+    )
+    meta = RIPMetadata(info, readables, writables)
+    return str(meta)
+
+  def buildSSEGetInfo(self):
+    return RIPMethod(
+      url='10.192.38.68:8080/RIP/SSE', 
+      description='Suscribes to an SSE to get regular updates on the servers\' variables',
+      type_='GET',
+      params=[
+        RIPParam(name='Accept',required='no',location='header',value='application/json'),
+        RIPParam(name='expId',required='yes',location='query',type_='string'),
+        RIPParam(name='variables',required='no',location='query',type_='array',subtype='string'),
+      ],
+      returns='text/event-stream',
+      example='10.192.38.68:8080/RIP/SSE?expId=TestOK'
+    )
+
+  def buildPOSTGetInfo(self):
+    elements = [{"description": "Experience id","type": "string"},
+    {"description": "Name of variables to be retrieved","type": "array","subtype": "string"}]
+    return RIPMethod(
+      url='10.192.38.68:8080/RIP/POST', 
+      description='Sends a request to retrieve the value of one or more servers\' variables on demand',
+      type_='POST',
+      params=[
+        RIPParam(name='Accept',required='no',location='header',value='application/json'),
+        RIPParam(name='Content-Type',required='yes',location='header',type_='application/json'),
+        RIPParam(name='jsonrpc',required='yes',type_='string',location='body',value="2.0"),
+        RIPParam(name='method',required='yes',type_='string',location='body',value='get'),
+        RIPParam(name='params',required='yes',type_='array',location='body',elements=elements),
+        RIPParam(name='id',required='yes',type_='int',location='body'),
+      ],
+      returns='application/json',
+      example={ "10.192.38.68:8080/RIP/POST": {
+        "headers": {"Accept": "application/json","Content-Type": "application/json"},
+        "body": {"jsonrpc":"2.0","method":"get","params":["TestOK",["var1","var2"]],"id":"1"}
+      }}
+    )
+
+  def buildPOSTSetInfo(self):
+    elements = [{"description": "Experience id","type": "string"},
+    {"description": "Name of variables to write","type": "array","subtype": "string"},
+    {"description": "Value for variables","type": "array","subtype": "mixed"}]
+    params_post_set = [
+      RIPParam(name='Accept',required='no',location='header',value='application/json'),
+      RIPParam(name='Content-Type',required='yes',location='header',type_='application/json'),
+      RIPParam(name='jsonrpc',required='yes',type_='string',location='body',value='2.0'),
+      RIPParam(name='method',required='yes',type_='string',location='body',value='set'),
+      RIPParam(name='params',required='yes',type_='array',location='body',elements=elements),
+      RIPParam(name='id',required='yes',type_='int',location='body'),
+      RIPParam(name='variables',required='no',location='query',type_='array',subtype='string'),
+    ]
+    example_post_set = {
+      "10.192.38.68:8080/RIP/POST": {
+        "headers": {"Accept": "application/json","Content-Type": "application/json"},
+        "body": {"jsonrpc":"2.0","method":"set","params":["TestOK",["var1","var2"],["val1","val2"]],"id":"1"}
+      }
+    }
+    return RIPMethod(
+      url='10.192.38.68:8080/RIP/POST', 
+      description='Sends a request to retrieve the value of one or more servers\' variables on demand',
+      type_='POST',
+      params=params_post_set,
+      returns='application/json',
+      example=example_post_set
+    )
+
+
+class Sampler(object):
+
+  def __init__(self, period):
+    self.Ts = period
+    self.reset()
+
+  def wait(self):
+    self.last = self.time
+    self.time = time.time() - self.t0
+    self.next = self.time / self.Ts + self.Ts
+    interval = self.Ts - self.time % self.Ts
+    time.sleep(interval)
+
+  def reset(self):
+    self.t0 = time.time()
+    self.time = 0
+    self.last = 0
+    self.next = self.Ts
+
+  def delta(self):
+    return self.time - self.last
+  
+  def lastTime(self):
+    return self.last
